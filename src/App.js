7dc9022cafb7e177588d0b8ffba7822612897c77
Francisco Bosch <franbosch43@gmail.com>
2022-11-15 12:06:45 -0300

    Aplicacion lista de tareas
    Archivos listos para usar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ListaDeTareas from './componentes/ListaDeTareas';
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+     <div className='tareas-lista-principal'>
+      <h1>Mis tareas</h1>
+      <ListaDeTareas />
+     </div>
+    </div>
+  );
+}
+
+export default App;
